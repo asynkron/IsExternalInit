76bf29a5defe73eec1f83e57fbebdc345d8ff30b
rogerjohansson <rogeralsing@gmail.com>
2020-10-30 18:31:45 +0100

    first

---
+++ IsExternalInit.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public sealed class IsExternalInit : Attribute
+    {
+    }
+}
